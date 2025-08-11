f4fe19630e7d6321a920b0ee8cdfa21c4fd5bcfa
dikablink (desktop) <director@gynseng.net>
2025-08-11 17:54:59 -0400

    chore(scripts): updated fspath within generate-sitemap.sh to the correct path

--- scripts/generate-sitemap.sh
+++ scripts/generate-sitemap.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 # Call generateSitemap.php within MediaWiki container
 docker compose exec -T mediawiki php /var/www/html/maintenance/generateSitemap.php \
   --server=https://wiki.anarkadia.net \
-  --fspath=/usr/share/nginx/html/sitemap \
+  --fspath=/data/sitemap \
   --urlpath=/sitemap
